21fa302c82e25f2abb85f79116ee01ce649b78e2
Aniruddha Dev Nath <113041453+ARNOB663@users.noreply.github.com>
2025-01-03 13:19:24 +0600

    update

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,8 +1,20 @@
+import { Link } from "react-router-dom"
 
 const Header = () => {
   return (
     <div>
-      <header>Header</header>
+      <header className="sticky top-0 z-50 w-full border-b bg-background/95">
+         <div>
+         <Link to={"/"} >
+         <img src="/logo.png" alt="CLOUD CAST Logo" className="h-14" />
+         </Link>
+         </div>
+         <div>
+            {/*search bar*/}
+            {/*theme toggle*/}
+         </div>
+
+      </header>
     </div>
   )
 }
