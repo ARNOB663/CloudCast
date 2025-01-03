d0edbe2d529fbbabafbd058c8a63f4038a0c2410
Aniruddha Dev Nath <113041453+ARNOB663@users.noreply.github.com>
2025-01-03 11:05:23 +0600

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import Layout from './components/layout'
 import { BrowserRouter } from 'react-router-dom'
+import  {ThemeProvider} from './context/theme-provider'
 import './App.css'
 
 
 
 function App() {
   return (
-    <BrowserRouter>
-    <Layout>Hello</Layout>
+   
+  <BrowserRouter>
+    
+     <ThemeProvider defaultTheme='dark'>
+    
+       <Layout>Hello</Layout>
+ 
+     </ThemeProvider>
+    
     </BrowserRouter>
   )
 }
